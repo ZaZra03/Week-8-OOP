febd218363bca25aa8f5861f4a4c008a049fb4c3
Ezra Micah Malsi <66657798+ZaZra03@users.noreply.github.com>
2022-10-06 14:21:45 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -23,41 +23,41 @@ public class Main {
 		int operation;
 		while (true) {
 			try {
-				System.out.println("------PHONE------");
+				System.out.println("\n------PHONE------");
+				System.out.println("");
 				System.out.println("\n[1] Buy Load");
-				System.out.println("[2] Text");
-				System.out.println("[3] Call");
-				System.out.println("[4] Change Number");
-				System.out.println("[5] Shut Down Phone\n");
+				System.out.println("[2] Check Balance");
+				System.out.println("[3] Text");
+				System.out.println("[4] Call");
+				System.out.println("[5] Change Number");
+				System.out.println("[6] Shut Down Phone\n");
 				System.out.print("Enter Operation: ");
 				operation = Integer.parseInt(in.readLine());
 				switch (operation) {
-					case 1: // Buy Load
+					case 1:
+						System.out.println("Enter amount: ");
+						int loadAmount = Integer.parseInt(in.readLine());
+						
 					case 2:
-						System.out.print("Enter the number of reciever: ");
-						String receiver = in.readLine();
-						System.out.print("Enter your message to: " + receiver + "\n");
-						String message = in.readLine();
-						objBasicPhone.Text(receiver, message);
-					case 3: // Call
-						while(true) {
-							try {
-								System.out.print("Enter the number which you would want to call: ");
-								int number = Integer.parseInt(in.readLine());
-								System.out.print("Enter the amount of time in seconds you would like to call: ");
-								int callLength = Integer.parseInt(in.readLine());
-								objBasicPhone.Call(String.valueOf(number), callLength);
-								break;
-							} catch (Exception e) {
-								// TODO: handle exception
-								System.out.println("Invalid input! Try again. ");
-							}
-						}
+						objBasicPhone.CheckBalance();
 						break;
-					case 4: // Change Number
-					case 5: // Shut Down Phone
-						System.out.println("Shutting Down");
-						Shutdown();
+						
+					case 3:
+							try {
+								System.out.print("Enter the number of reciever: ");
+								int receiver = Integer.parseInt(in.readLine());
+								System.out.print("Enter your message to: " + receiver + "\n");
+								String message = in.readLine();
+								objBasicPhone.Text(String.valueOf(receiver), message);
+								break;
+								
+							} catch (Exception e) {
+								System.out.println("Invalid input! Try again.");
+							}
+							
+					case 4: 
+					case 5: 
+					case 6: 
 				}
 			} catch (Exception e) {
 				System.out.println("Error");
